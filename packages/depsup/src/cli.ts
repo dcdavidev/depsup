fb1aa169cc91f3258cfc2a2e2ed3bdf6a207cd00
Davide Di Criscito <davidedc@outlook.com>
2025-11-11 16:50:55 +0100

    feat: use commander for cli argument parsing

--- packages/depsup/src/cli.ts
+++ packages/depsup/src/cli.ts
@@ -1,15 +1,20 @@
+import { Command } from 'commander';
+
 import { depsup } from './depsup.js';
 import { getErrorMessage } from './lib/get-error-message.js';
 import { logError } from './logger.js';
 
-const args = new Set(process.argv.slice(2));
-const yes = args.has('--yes') || args.has('-y');
+const program = new Command();
 
-(async () => {
-  try {
-    await depsup({ yes });
-  } catch (error: unknown) {
-    logError(getErrorMessage(error));
-    process.exit(1);
-  }
-})();
+program
+  .option('-y, --yes', 'Skip interactive mode and confirm all prompts', false)
+  .action(async (options) => {
+    try {
+      await depsup({ yes: options.yes });
+    } catch (error: unknown) {
+      logError(getErrorMessage(error));
+      process.exit(1);
+    }
+  });
+
+program.parse(process.argv);
